33f070331638d1e8282a872d8d5746d5949e7643
AaronI3 <aaron.horne@innovationind.com>
2023-10-11 11:45:08 -0500

    autocomplete field will only display values that match typed term

--- src/app/component/autocomplete/autocomplete.component.ts
+++ src/app/component/autocomplete/autocomplete.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.css']
 })
-export class AutocompleteComponent {
-
+export class AutocompleteComponent implements OnInit{
   colorArray = ['Red','Green','Blue'];
+  filteroptions!:Observable<string[]>;
+  formcontrol=new FormControl('');
+  
+  ngOnInit(): void {
+    this.filteroptions=this.formcontrol.valueChanges.pipe(
+      startWith(''),map(value=>this._FILTER(value|| ''))
+    )
+  }
+
+  private _FILTER(value:string):string[]{
+    const searchvalue = value.toLocaleLowerCase();
+    return this.colorArray.filter(option=>option.toLocaleLowerCase().includes(searchvalue));
+  }
+
+
 }
