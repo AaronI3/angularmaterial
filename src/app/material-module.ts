13f9b62517cb9ac3a10a90acfbc6bab645a9d853
AaronI3 <aaron.horne@innovationind.com>
2023-10-12 09:16:19 -0500

    created menu toolbar at the top of the page

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input"
 import { MatAutocompleteModule } from "@angular/material/autocomplete"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatMenuModule } from "@angular/material/menu"
+import { MatIconModule } from "@angular/material/icon"
+import { MatButtonModule } from "@angular/material/button"
 
 @NgModule({
 exports:[
     MatInputModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
 ]
 }) 
 
