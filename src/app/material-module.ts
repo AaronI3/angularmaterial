93153f5ff45bd56b6a3c2181754ee46e8c2bf264
AaronI3 <aaron.horne@innovationind.com>
2023-10-13 10:00:55 -0500

    side nave bar is displaying

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input"
 import { MatAutocompleteModule } from "@angular/material/autocomplete"
-
 import { MatMenuModule } from "@angular/material/menu"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatIconModule } from "@angular/material/icon"
 import { MatButtonModule } from "@angular/material/button"
 import { MatBadgeModule } from "@angular/material/badge"
 import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatListModule } from "@angular/material/list"
 
 @NgModule({
 exports:[
@@ -18,8 +18,8 @@ exports:[
     MatToolbarModule,
     MatMenuModule,
     MatBadgeModule,
-    
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule
 ]
 }) 
 
