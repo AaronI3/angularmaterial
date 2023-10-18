72f03ed11bef37077fc84bf5d5bea01ab093943d
AaronI3 <aaron.horne@innovationind.com>
2023-10-18 15:04:16 -0500

    importing httpclient for backend stuff

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -14,6 +14,7 @@ import { MatSliderModule } from '@angular/material/slider'
 import { MatTableModule } from '@angular/material/table'
 import { MatSortModule } from '@angular/material/sort'
 import { MatPaginatorModule } from '@angular/material/paginator'
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
 exports:[
@@ -31,7 +32,8 @@ exports:[
     MatSliderModule,
     MatTableModule,
     MatSortModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    HttpClientModule,
 ]
 }) 
 
