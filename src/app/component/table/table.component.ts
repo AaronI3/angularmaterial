f95b17ae48822764a7ff7172e614c3af0db3eda3
AaronI3 <aaron.horne@innovationind.com>
2023-10-19 14:21:42 -0500

    End of Mat-Table video

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Customer } from 'src/app/Model/Customer';
 import { MasterService } from 'src/app/service/master.service';
@@ -13,13 +15,20 @@ export class TableComponent {
   customerList !: Customer[];
   dataSource : any;
   displayedColumns:string[]=['code','name','email','phone','status','action'];
-
-
+  @ViewChild(MatPaginator) pagination !:MatPaginator;
+  @ViewChild(MatSort) sort !:MatSort;
 
   constructor(private service:MasterService){
     this.service.GetCustomer().subscribe(res=>{
       this.customerList = res;
       this.dataSource= new MatTableDataSource<Customer>(this.customerList);
+      this.dataSource.paginator = this.pagination;
+      this.dataSource.sort = this.sort;
     });
+  }
+
+  FilterChange(data:Event){
+    const value=(data.target as HTMLInputElement).value;
+    this.dataSource.filter=value;
   }
 }
