3372d4b5c00e24985380eb7c3bbb19f749ebb01d
AaronI3 <aaron.horne@innovationind.com>
2023-10-20 14:28:58 -0500

    implemented datepicker

--- src/app/component/formdesign/formdesign.component.ts
+++ src/app/component/formdesign/formdesign.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class FormdesignComponent {
 
   countryList=['USA','RUSSIA','CHINA','GREAT BRITIAN']
+  termlist=['1001 Days','2234 days','3123 days','4123 days','5123 days']
 }
