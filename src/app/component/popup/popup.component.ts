8417207adfdc31dc03d0e9f4fa8434b794315265
AaronI3 <aaron.horne@innovationind.com>
2023-10-23 15:58:34 -0500

    can be saved now, grayed out save if required fields not filled

--- src/app/component/popup/popup.component.ts
+++ src/app/component/popup/popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -10,7 +11,11 @@ export class PopupComponent implements OnInit {
   inputdata:any;
   closemessage='closed using directive';
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private ref:MatDialogRef<PopupComponent>){
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data:any,
+    private ref:MatDialogRef<PopupComponent>,
+    private builder:FormBuilder,
+    ){
 
   }
   ngOnInit(): void {
@@ -19,5 +24,16 @@ export class PopupComponent implements OnInit {
 
   closePopup(){
     this.ref.close();
+  }
+
+  myForm = this.builder.group({
+    name:this.builder.control('',Validators.required),
+    email:this.builder.control('',Validators.required),
+    phone:this.builder.control('',Validators.required),
+    status:this.builder.control(true,Validators.required),
+  });
+
+  Saveuser(){
+    console.log(this.myForm.value);
   }
 }
