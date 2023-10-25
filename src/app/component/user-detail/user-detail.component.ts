c85c80f76a5199be95a9890aec1e115ade5028dc
AaronI3 <aaron.horne@innovationind.com>
2023-10-25 16:04:44 -0500

    suff

--- src/app/component/user-detail/user-detail.component.ts
+++ src/app/component/user-detail/user-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MasterService } from 'src/app/service/master.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit{
 
+  constructor(
+    private service:MasterService,
+    private buildr:FormBuilder,
+    ){
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
+  customerDetails:any;
+  detailsForm = this.buildr.group({
+    name: this.buildr.control('')
+    name: this.buildr.control('')
+    name: this.buildr.control('')
+    name: this.buildr.control('')
+    })
+
+  
 }
