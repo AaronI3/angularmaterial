4218d8036228b1d6fa3bd99413b8bcf6c9607e8c
AaronI3 <aaron.horne@innovationind.com>
2023-10-26 15:56:20 -0500

    chat gpt stuff

--- src/app/component/user-detail/user-detail.component.ts
+++ src/app/component/user-detail/user-detail.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MasterService } from 'src/app/service/master.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,25 +8,40 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit{
+  
+  //will be assigned the injected data
+  customerDetails:any;
 
+  //will be assigned the data from the database
+  display:any;
+
+  //will be assigned the close message
+  closemessage='Closed with Function';
+  
   constructor(
+    //injects the data from the parent component
+    @Inject(MAT_DIALOG_DATA) public data:any,
+
+    //injects the service to be used
     private service:MasterService,
-    private buildr:FormBuilder,
+
+    //injects the dialog reference to be used to close the popup
+    private ref:MatDialogRef<UserDetailComponent>
     ){
 
   }
 
-
+  //closes the popup
   ngOnInit(): void {
-
+    this.customerDetails = this.data;
+    if (this.customerDetails.code > 0){
+      this.service.GetCustomerByCode(this.customerDetails.code).subscribe(item=>{
+        this.display = item;
+      })
+    }
   }
-  customerDetails:any;
-  detailsForm = this.buildr.group({
-    name: this.buildr.control('')
-    name: this.buildr.control('')
-    name: this.buildr.control('')
-    name: this.buildr.control('')
-    })
+  
+
 
   
 }
